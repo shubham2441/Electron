c00e91d4ff7037589411c65dc9c404c510c7fcd7
Shubham Chauhan <shubhamchauhan0304@gmail.com>
2020-07-01 14:24:38 +0530

    Electron

---
+++ main.js
@@ -0,0 +1,10 @@
+const electron = require("electron");
+const app = electron.app;
+function createWindow() {
+    const win = new electron.BrowserWindow({
+        width: 800,
+        height: 600
+    })
+}
+
+app.whenReady().then(createWindow);
